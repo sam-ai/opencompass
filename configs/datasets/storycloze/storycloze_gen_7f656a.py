7645bcf63bbc9433a47e229a0a6677b9c99b6260
sam-ai <sambitsekharai@gmail.com>
2023-10-21 01:42:56 +0530

    story_cloz added

--- configs/datasets/storycloze/storycloze_gen_7f656a.py
+++ configs/datasets/storycloze/storycloze_gen_7f656a.py
@@ -17,7 +17,8 @@ storycloze_infer_cfg = dict(
             dict(
                 role="HUMAN",
                 prompt=
-                "{context}\nQuestion: Which ending makes the most sense?\nA. {sentence_quiz1}\nB. {sentence_quiz2}\nYou may choose between 'A' and 'B'.\nAnswer:",
+                # "{context}\nQuestion: Which ending makes the most sense?\nA. {sentence_quiz1}\nB. {sentence_quiz2}\nYou may choose between 'A' and 'B'.\nAnswer:",
+                "### System:\nBelow is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n\n\n### Instruction:\nप्रश्न: कौन सा अंत सबसे अधिक अर्थपूर्ण है?\nA. {sentence_quiz1}\nB. {sentence_quiz2}\nआप इनमें से चुन सकते हैं 'A' और 'B'.\n\n### Input:\n{context}\n\n### Response:\n"
             ),
         ]),
     ),
@@ -37,8 +38,8 @@ storycloze_datasets = [
     dict(
         abbr="story_cloze",
         type=storyclozeDataset_V2,
-        path="juletxara/xstory_cloze",
-        name="en",
+        path="sam2ai/hindi_story_cloze_mini",
+        # name="en",
         reader_cfg=storycloze_reader_cfg,
         infer_cfg=storycloze_infer_cfg,
         eval_cfg=storycloze_eval_cfg,
--- configs/datasets/storycloze/storycloze_ppl_496661.py
+++ configs/datasets/storycloze/storycloze_ppl_496661.py
@@ -15,7 +15,7 @@ storycloze_infer_cfg = dict(
         type=PromptTemplate,
         template={
             i: dict(round=[
-                dict(role="HUMAN", prompt="{context}"),
+                dict(role="HUMAN", prompt="### System:\nBelow is an instruction that describes a task. Write a response that appropriately completes the request.\n\n\n\n### Instruction:\n{context}\n\n### Response:\n"),
                 dict(role="BOT", prompt=f"{{sentence_quiz{i}}}"),
             ])
             for i in range(1, 3)
@@ -31,8 +31,8 @@ storycloze_datasets = [
     dict(
         abbr='story_cloze',
         type=storyclozeDataset,
-        path='juletxara/xstory_cloze',
-        name='en',
+        path='sam2ai/hindi_story_cloze_mini',
+        # name='en',
         reader_cfg=storycloze_reader_cfg,
         infer_cfg=storycloze_infer_cfg,
         eval_cfg=storycloze_eval_cfg)
